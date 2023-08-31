fa10c5d66669ecaf1ea29347dd49f4c4f6a3126b
HanSte <stecherthananja@outlook.de>
2023-08-31 19:36:07 +0200

    new

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ function hschangeContent(hscontentId) {
     var hsnewContent = document.getElementById(hscontentId).innerHTML;
     hscontent.innerHTML = hsnewContent;
 }
+
+document.getElementById("hs-media-search-input").addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) { // Überprüfen, ob die Eingabetaste gedrückt wurde
+      event.preventDefault(); // Verhindern, dass das Formular gesendet wird
+      hsmediasearchcontent(); // Funktion zum Durchführen der Suche aufrufen
+    }
+});
+
+function hsmediasearchcontent() {
+    var hscontent = document.getElementById('hs-media-content');
+    var hsmediaserchcontentb = document.getElementById('hs-media-search-content-b');
+    hscontent.innerHTML = hsmediaserchcontentb;
+}
