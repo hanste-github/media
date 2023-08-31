dd1112d06f19604185a6d40e90b92e7b9298253f
HanSte <stecherthananja@outlook.de>
2023-08-31 20:05:18 +0200

    new

--- index.js
+++ index.js
@@ -15,4 +15,18 @@ function hsmediasearchcontent() {
     var hscontent = document.getElementById('hs-media-content');
     var hsmediasearchcontentb = document.getElementById('hs-media-search-content-b').innerHTML;
     hscontent.innerHTML = hsmediasearchcontentb;
+
+    var input = document.getElementById('hs-media-search-input');
+    var filter = input.value.toUpperCase();
+    var ul = document.getElementById('hs-media-search-ul');
+    var li = ul.getElementsByTagName('li');
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
 }
