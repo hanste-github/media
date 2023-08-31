f457681f86ccdd7f9a24646b0b11a00befed4ca0
HanSte <stecherthananja@outlook.de>
2023-08-31 14:25:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function changeContent(hscontentId) {
+function hschangeContent(hscontentId) {
     var hscontent = document.getElementById('hs-media-content');
     var hsnewContent = document.getElementById(hscontentId).innerHTML;
     hscontent.innerHTML = hsnewContent;
